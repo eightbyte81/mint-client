f2935c7cbd6f6b86a4ae711012364e0f225c22e2
Daniil Lyudvig <thricht3r@yandex.ru>
2022-04-12 02:54:01 +0300

    Added login page content

--- src/components/AuthPage/LoginPage/LoginPage.js
+++ src/components/AuthPage/LoginPage/LoginPage.js
@@ -1,5 +1,81 @@
+import {useState} from "react";
+import {login} from "../../../api/authService";
+
 export const LoginPage = () => {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleSubmit = async e => {
+        e.preventDefault()
+        const token = await login({username, password})
+        console.log(token)
+        e.target.reset()
+    }
+
     return (
-        <h1>Login Page</h1>
+        <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+            <div className="max-w-md w-full space-y-8">
+                <div>
+                    <img
+                        className="mx-auto h-12 w-auto"
+                        src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                        alt="MINT"
+                    />
+                    <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Войдите в аккаунт</h2>
+                </div>
+                <form className="mt-8 space-y-6" onSubmit={handleSubmit} method="POST">
+                    <input type="hidden" name="remember" defaultValue="true" />
+                    <div className="rounded-md shadow-sm -space-y-px">
+                        <div>
+                            <label htmlFor="username" className="sr-only">
+                                Имя пользователя
+                            </label>
+                            <input
+                                id="username"
+                                name="username"
+                                type="username"
+                                onChange={e => setUsername(e.target.value)}
+                                autoComplete="username"
+                                required
+                                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-teal-accent-400 focus:border-teal-accent-400 focus:z-10 sm:text-sm"
+                                placeholder="Имя пользователя"
+                            />
+                        </div>
+                        <div>
+                            <label htmlFor="password" className="sr-only">
+                                Пароль
+                            </label>
+                            <input
+                                id="password"
+                                name="password"
+                                type="password"
+                                onChange={e => setPassword(e.target.value)}
+                                autoComplete="current-password"
+                                required
+                                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-teal-accent-400 focus:border-teal-accent-400 focus:z-10 sm:text-sm"
+                                placeholder="Пароль"
+                            />
+                        </div>
+                    </div>
+
+                    <div className="flex items-end justify-end">
+                        <div className="text-sm">
+                            <a href="/" className="font-medium text-teal-accent-400 hover:text-teal-accent-500">
+                                Забыли пароль?
+                            </a>
+                        </div>
+                    </div>
+
+                    <div>
+                        <button
+                            type="submit"
+                            className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-teal-accent-400 hover:bg-teal-accent-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500"
+                        >
+                            Войти
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
     )
 }
